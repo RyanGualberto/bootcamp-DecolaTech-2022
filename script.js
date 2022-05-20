6ba0e8968fb8537089033559e3ef3b098ad0b110
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-05-20 00:15:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-function calculaIdade(anos) {
-	return `Daqui a ${anos} anos, ${this.nome} terá ${
-		this.idade + anos
-	} anos de idade.`;
+const alunos = [
+	{
+		nome: 'João',
+		nota: 5,
+		turma: '1B',
+	},
+	{
+		nome: 'Sofia',
+		nota: 9,
+		turma: '1B',
+	},
+	{
+		nome: 'Paulo',
+		nota: 6,
+		turma: '2C',
+	},
+    {
+		nome: 'Felipe',
+		nota: 2,
+		turma: '1C',
+	},
+];
+
+function alunosAprovados(alunos, media) {
+	let aprovados = [];
+
+	for (let i = 0; i < alunos.length; i++) {
+		let { nota, nome } = alunos[i];
+
+		if (nota >= media) {
+			aprovados.push(nome);
+		}   
+	}
+
+	return aprovados;
 }
 
-const pessoa1 = {
-	nome: 'Amelia',
-	idade: 23,
-};
-
-const pessoa2 = {
-	nome: 'Márcia',
-	idade: 20,
-};
-
-const pessoa3 = {
-	nome: 'Jonas',
-	idade: 13,
-};
-
-console.log(calculaIdade.call(pessoa3, 40));
-console.log(calculaIdade.apply(pessoa2, [24]));
+console.log(alunosAprovados(alunos, 5));
